f4cad6f89eddbb7264cb59c5c8d885f8348199cb
Alexberemart <alexberemart@gmail.com>
2016-11-19 14:43:39 +0100

    refactoring

--- src/main/java/com/alexberemart/homeAccounting/config/DataSourceConfig.java
+++ src/main/java/com/alexberemart/homeAccounting/config/DataSourceConfig.java
@@ -1,32 +1,73 @@
 package com.alexberemart.homeAccounting.config;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-@PropertySource("classpath:/data/jdbc-dev.properties")
 public class DataSourceConfig {
 
-    @Autowired private Environment env;
-
-    @Bean(destroyMethod="close")
-    public DataSource dataSource() {
+    @Bean(name = "dataSource", destroyMethod = "close")
+    public BasicDataSource getBasicDataSource(
+            @Value("${core.datasource.driver}") String driver,
+            @Value("${core.datasource.url}") String url,
+            @Value("${core.datasource.db.username}") String username,
+            @Value("${core.datasource.db.password}") String password,
+            @Value("${core.datasource.show.sql}") Boolean testOnBorrow,
+            @Value("${core.datasource.validationQuery}") String validationQuery) {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.username"));
-        dataSource.setPassword(env.getProperty("jdbc.password"));
-        dataSource.setMaxActive(env.getProperty("dbcp.maxActive", Integer.class));
-        dataSource.setMaxIdle(env.getProperty("dbcp.maxIdle", Integer.class));
-        dataSource.setMaxWait(env.getProperty("dbcp.maxWait", Integer.class));
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setValidationQuery(validationQuery);
         return dataSource;
     }
 
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean getSessionFactory(
+            BasicDataSource dataSource,
+            @Value("${hibernate.dialect}") String dialect) {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource);
+        localSessionFactoryBean.getHibernateProperties().setProperty("hibernate.hbm2ddl.auto", "update");
+        localSessionFactoryBean.getHibernateProperties().setProperty("hibernate.dialect", dialect);
+        return localSessionFactoryBean;
+    }
+
+    @Bean(name = "jpaVendorAdapter")
+    public HibernateJpaVendorAdapter getJpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(true);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean getLocalContainerEntityManagerFactoryBean(
+            BasicDataSource dataSource,
+            HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("com.alexberemart");
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getHibernateTransactionManager(
+            SessionFactory sessionFactory) {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory);
+        return hibernateTransactionManager;
+    }
 }
