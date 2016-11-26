6e5516ac6f7270c9369a41d1d7502edd6ecbba85
Alexberemart <alexberemart@gmail.com>
2016-11-26 16:39:31 +0100

    start to create front-end

--- src/main/java/com/alexberemart/homeAccounting/Application.java
+++ src/main/java/com/alexberemart/homeAccounting/Application.java
@@ -2,13 +2,21 @@ package com.alexberemart.homeAccounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan("com.alexberemart")
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
 }
